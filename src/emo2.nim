5436adcdcdd93507f1bc46d9d56b78dd48781bac
Leon Schumacher <leonsch@protonmail.com>
2022-05-15 19:06:26 +0200

    clients clear on disconnect

--- src/emo2.nim
+++ src/emo2.nim
@@ -7,9 +7,60 @@ proc handle(client: AsyncSocket) {.async.} =
   var position: Position = nil # the current playlist position = the most recently emitted song
   defer: client.close
 
+  proc finish =
+    ## Leave current song, possible deleting it if we were the last referent and
+    ## and the song is the last element in the playlist
+    # we have an old element, decrease its refcount
+    dec position.value.refcount
+
+    # if old element is not referenced anymore and
+    # is the start of the playlist, delete it
+    if position.value.refcount <= 0 and position == globalList.head:
+      globalList.delHead
+
+  proc advance =
+    ## Advances `position` to the next song in the playlist.
+    ## If there is no current song selected, go to the start of the playlist.
+    ## If `generateNewSong` is false, no new song will be generated
+    ## when `position` is at the end of the playlist.
+
+    if position == nil:
+      # go to start of playlist
+      position = globalList.head
+      if position == nil:
+        # playlist is empty, add new element
+        globalList.addFromGenerator
+        position = globalList.head
+    else:
+      finish() # finish current song
+
+      # possibly generate a new song
+      if position.next == nil:
+        globalList.addFromGenerator
+
+      # switch to next song
+      position = position.next
+
+    # at this point, we are at the next song, so we increase its refcount
+    inc position.value.refcount
+
+  proc clearCmd =
+    # potentially join queue
+    if position == nil:
+      advance()
+
+    # go to the last element
+    while position.next != nil:
+      advance()
+
   while true:
+    defer: echo globalList
+
     let line = await client.recvLine
     if line == "": # client closed the connection
+      # decrease refcount of current song, process queue cleanup
+      clearCmd()
+      finish()
       break
     elif line == "\r\L": # empty line received
       continue
@@ -23,38 +74,6 @@ proc handle(client: AsyncSocket) {.async.} =
       ## Simulate sending a line by appending it to the cache (`res`).
       ## The given line must not end with a line terminator (it will be added by this function).
       res.add line & "\n"
-
-    proc advance =
-      ## Advances `position` to the next song in the playlist.
-      ## If there is no current song selected, go to the start of the playlist.
-      ## If `generateNewSong` is false, no new song will be generated
-      ## when `position` is at the end of the playlist.
-
-      if position == nil:
-        # go to start of playlist
-        position = globalList.head
-        if position == nil:
-          # playlist is empty, add new element
-          globalList.addFromGenerator
-          position = globalList.head
-      else:
-        # we have an old element, decrease its refcount
-        dec position.value.refcount
-
-        # if old element is not referenced anymore and
-        # is the start of the playlist, delete it
-        if position.value.refcount <= 0 and position == globalList.head:
-          globalList.delHead
-
-        # possibly generate a new song
-        if position.next == nil:
-          globalList.addFromGenerator
-
-        # switch to next song
-        position = position.next
-
-      # at this point, we are at the next song, so we increase its refcount
-      inc position.value.refcount
 
     proc nextCmd =
       advance()
@@ -80,13 +99,7 @@ proc handle(client: AsyncSocket) {.async.} =
       nextCmd()
 
     of "clear":
-      # potentially join queue
-      if position == nil:
-        advance()
-
-      # go to the last element
-      while position.next != nil:
-        advance()
+      clearCmd()
 
     of "complete":
       if args.len == 0:
