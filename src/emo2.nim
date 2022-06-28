b8592acc2e52da76440e98d7d6ffdc3c4eb4311f
Leon Schumacher <leonsch@protonmail.com>
2022-06-29 00:20:41 +0200

    Complete zeolite communication

--- src/emo2.nim
+++ src/emo2.nim
@@ -1,24 +1,15 @@
-import std/[asyncnet, asyncdispatch, strutils, ropes, lists, db_sqlite]
+import std/[strutils, ropes, lists, db_sqlite, options]
 import playlist
 import zeolite
 
-var z: zeolite
 var globalList: Playlist
 
 proc trustAll(pk: zeolite.sign_pk): zeolite.error =
   echo pk
   return zeolite.SUCCESS
 
-proc handle(client: AsyncSocket) {.async.} =
-  var c: zeolite.channel
+proc handler(coro: ptr int, loop: ptr int, channel: ptr zeolite.channel): cint =
   var position: Position = nil # the current playlist position = the most recently emitted song
-  defer: client.close
-
-  if zeolite.create_channel(
-    addr z, addr c, cast[cint](client.getFd), trustAll
-  ) != zeolite.SUCCESS:
-    echo "Could not create channel"
-    return
 
   proc finish =
     ## Leave current song, possible deleting it if we were the last referent and
@@ -66,112 +57,126 @@ proc handle(client: AsyncSocket) {.async.} =
     while position.next != nil:
       advance()
 
-  while true:
-    defer: echo globalList
-
+  proc recvLine: Option[string] =
     var buf: cstring
-    var len: csize_t
-    if zeolite.channel_recv(addr c, addr buf, addr len) != zeolite.SUCCESS:
+    var len: uint32
+
+    if zeolite.channel_recv(coro, channel, addr buf, addr len) != zeolite.SUCCESS:
+      echo "Could not receive line"
       # client closed the connection
       # decrease refcount of current, process queue cleanup
       clearCmd()
       finish()
-      break
-
-    if len < 2: continue
-
-    # copy to string, remove last character (newline)
-    var line = newString(len - 1)
-    copyMem(addr line[0], buf, len - 1)
-
-    let parts = line.split(" ", maxsplit = 1)
-    let cmd   = parts[0]
-    let arg   = if parts.len > 1: parts[1] else: ""
-
-    var res: Rope
-    proc sendLine(line: string) =
-      ## Simulate sending a line by appending it to the cache (`res`).
-      ## The given line must not end with a line terminator (it will be added by this function).
-      res.add line & "\n"
-
-    proc nextCmd =
-      advance()
-      sendLine position.value.song
-
-    case cmd
-    of "queue":
-      if position == nil:
-        for i in globalList: sendLine "queued " & $i
-      else:
-        for i in position.itemsFrom: sendLine "queued " & $i
-      sendLine "end"
-
-    of "add":
-      if arg.len == 0:
-        sendLine "error args"
-      else:
-        globalList.addSong arg
-        sendLine "added " & arg
-
-    of "next":
-      nextCmd()
-
-    of "clear":
-      clearCmd()
-
-    of "complete":
-      if arg.len == 0:
-        sendLine "error args"
-      else:
-        complete arg
-
-    of "getTable":
-      if arg.len == 0:
-        sendLine "error args"
-      else:
-        try:
-          for row in db.fastRows(sql "select * from " & arg):
-            sendLine row.join "\t"
-          sendLine ""
-        except DbError:
-          sendLine "error " & getCurrentExceptionMsg()
-
-    of "mergeChanges":
-      while true:
-        let change = await client.recvLine
-        if change == "" or change == "\r\L":
-          break
-
-        let parts = change.split "\t"
-        let song  = parts[0]
-        let diff  = parts[1].parseInt
-        db.exec(sql "update songs set count = count + ? where path = ?", diff, song)
-
-    else:
-      echo "unknown command ", line
-      sendLine "error unknown"
-
-    if zeolite.channel_send(addr c, cstring($res), cast[csize_t](res.len)) != zeolite.SUCCESS:
-      echo "Could not send line"
       return
 
-proc main {.async.} =
-  let server = newAsyncSocket()
-  server.setSockOpt(OptReuseAddr, true)
-  server.bindAddr 37812.Port
-  server.listen
+    var line = newString(len)
+    copyMem(addr line[0], buf, len)
+    result = some(line)
 
+  block main:
+    while true:
+      defer: echo globalList
+
+      let optLine = recvLine()
+      if optLine.isNone:
+        break main
+      let line = optLine.unsafeGet
+
+      if line.len < 2: continue
+
+      let parts = line.split(" ", maxsplit = 1)
+      let cmd   = parts[0]
+      let arg   = if parts.len > 1: parts[1] else: ""
+
+      var res: Rope
+      proc sendLine(line: string) =
+        ## Simulate sending a line by appending it to the cache (`res`).
+        ## The given line must not end with a line terminator (it will be added by this function).
+        res.add line & "\n"
+
+      proc nextCmd =
+        advance()
+        sendLine position.value.song
+
+      case cmd
+      of "queue":
+        if position == nil:
+          for i in globalList: sendLine "queued " & $i
+        else:
+          for i in position.itemsFrom: sendLine "queued " & $i
+        sendLine "end"
+
+      of "add":
+        if arg.len == 0:
+          sendLine "error args"
+        else:
+          globalList.addSong arg
+          sendLine "added " & arg
+
+      of "next":
+        nextCmd()
+
+      of "clear":
+        clearCmd()
+
+      of "complete":
+        if arg.len == 0:
+          sendLine "error args"
+        else:
+          complete arg
+
+      of "getTable":
+        if arg.len == 0:
+          sendLine "error args"
+        else:
+          try:
+            for row in db.fastRows(sql "select * from " & arg):
+              sendLine row.join "\t"
+            sendLine ""
+          except DbError:
+            sendLine "error " & getCurrentExceptionMsg()
+
+      of "mergeChanges":
+        while true:
+          let optChange = recvLine()
+          if optChange.isNone:
+            break main
+          let change = optChange.unsafeGet
+
+          if change == "" or change == "\r\L":
+            break
+
+          let parts = change.split "\t"
+          let song  = parts[0]
+          let diff  = parts[1].parseInt
+          db.exec(sql "update songs set count = count + ? where path = ?", diff, song)
+
+      else:
+        echo "unknown command ", line
+        sendLine "error unknown"
+
+      if zeolite.channel_send(coro, channel, cstring($res), cast[uint32](res.len)) != zeolite.SUCCESS:
+        echo "Could not send line"
+        return
+
+proc main =
   if zeolite.init() != 0:
     quit "Could not load zeolite!"
 
+  var z: zeolite
   if zeolite.create(addr z) != zeolite.SUCCESS:
     quit "Could not create zeolite identity!"
 
   echo "emo2: ready for connections!"
-  while true:
-    let client = await server.accept
-    echo "accepted client"
-    asyncCheck client.handle
+  {.emit: ["""
+    zeolite_multiServer(
+      """, z.addr, """,
+      "localhost",
+      "37812",
+      """, trustAll, """,
+      """,handler, """
+    );
+  """].}
 
-asyncCheck main()
-runForever()
+{.passL: "-lzeolite -lkrimskrams" .}
+main()
