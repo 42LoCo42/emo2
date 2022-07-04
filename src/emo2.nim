a8fe63d42b64cf2a20a3345bc344662e137e9826
Leon Schumacher <leonsch@protonmail.com>
2022-07-04 21:08:49 +0200

    TODO C rewrite

--- src/emo2.nim
+++ src/emo2.nim
@@ -1,7 +1,8 @@
-import std/[os, strutils, lists, db_sqlite]
-import playlist
+import std/[strutils, db_sqlite, os]
 
-var globalList: Playlist
+let db = open("songs.db", "", "", "")
+
+type send = proc(coro: pointer, channel: pointer, msg: cstring)
 
 {.compile: "main.c".}
 {.passL: "-lsodium -lkrimskrams -lzeolite".}
@@ -10,76 +11,7 @@ var globalList: Playlist
 proc entrypoint(address: cstring, port: cstring)
 {.pop.}
 
-proc finish(pos: Position) =
-  ## Leave current song, possible deleting it if we were the last referent and
-  ## and the song is the last element in the playlist
-  # we have an old element, decrease its refcount
-  dec pos.value.refcount
-
-  # if old element is not referenced anymore and
-  # is the start of the playlist, delete it
-  if pos.value.refcount <= 0 and pos == globalList.head:
-    globalList.delHead
-
-proc advance(pos: var Position) =
-  ## Advances `position` to the next song in the playlist.
-  ## If there is no current song selected, go to the start of the playlist.
-  ## If `generateNewSong` is false, no new song will be generated
-  ## when `position` is at the end of the playlist.
-
-  if pos == nil:
-    # go to start of playlist
-    pos = globalList.head
-    if pos == nil:
-      # playlist is empty, add new element
-      globalList.addFromGenerator
-      pos = globalList.head
-  else:
-    finish(pos) # finish current song
-
-    # possibly generate a new song
-    if pos.next == nil:
-      globalList.addFromGenerator
-
-    # switch to next song
-    pos = pos.next
-
-  # at this point, we are at the next song, so we increase its refcount
-  inc pos.value.refcount
-
-type send = proc(data: pointer, song: cstring)
-
 {.push exportc.}
-proc printList =
-  echo "Playlist:"
-  for item in globalList:
-    echo "\t" & $item
-
-proc addToPlaylist(song: cstring) =
-  globalList.addSong $song
-
-proc queue(data: pointer, pos: Position, send: send) =
-  if pos == nil:
-    for item in globalList:
-      send(data, cstring("queued " & item.song & "\n"))
-  else:
-    for item in pos.itemsFrom:
-      send(data, cstring("queued " & item.song & "\n"))
-  send(data, "end\n")
-
-proc clear(pos: var Position) =
-  # potentially join queue
-  if pos == nil:
-    advance(pos)
-
-  # go to the last element
-  while pos.next != nil:
-    advance(pos)
-
-proc next(data: pointer, pos: var Position, send: send) =
-  advance(pos)
-  send(data, cstring(pos.value.song & "\n"))
-
 proc complete(song: cstring) =
   echo "completing ", song
   let count = db.getValue(sql "select count from songs where path = ?", song)
@@ -88,13 +20,13 @@ proc complete(song: cstring) =
   else:
     db.exec(sql "update songs set count = ? + 1 where path = ?", count, song)
 
-proc getTable(data: pointer, table: cstring, send: send) =
+proc getTable(send: send, coro: pointer, channel: pointer, table: cstring) =
   try:
     for row in db.fastRows(sql "select * from " & $table):
-      send(data, cstring(row.join("\t") & "\n"))
-    send(data, "\n")
+      send(coro, channel, cstring(row.join("\t") & "\n"))
+    send(coro, channel, "\n")
   except DbError:
-    send(data, cstring("error " & getCurrentExceptionMsg() & "\n"))
+    send(coro, channel, cstring("error " & getCurrentExceptionMsg() & "\n"))
 
 proc mergeChanges(data: pointer, recv: proc(data: pointer): cstring) =
   while true:
@@ -115,15 +47,12 @@ proc mergeChanges(data: pointer, recv: proc(data: pointer): cstring) =
     let diff  = parts[1].parseInt
     db.exec(sql "update songs set count = count + ? where path = ?", diff, song)
 
-proc onDisconnect(pos: var Position) =
-  clear(pos)
-  finish(pos)
+# proc onDisconnect(pos: var Position) =
+#   clear(pos)
+#   finish(pos)
 {.pop.}
 
-proc main =
-  let args = commandLineParams()
-  if args.len < 2:
-    quit "Usage: $1 address port" % [getAppFilename()]
-  entrypoint(cstring(args[0]), cstring(args[1]))
-
-main()
+let args = commandLineParams()
+if args.len < 2:
+  quit "Usage: $1 address port" % [getAppFilename()]
+entrypoint(cstring(args[0]), cstring(args[1]))
--- src/main.c
+++ src/main.c
@@ -9,32 +9,15 @@
 #define ERR_NEEDS_ARG "error: This function needs an argument!\n"
 #define LEN_NEEDS_ARG strlen(ERR_NEEDS_ARG)
 
-#define needsArg(msg)                               \
-	if(arg == NULL) {                               \
-		zeolite_channel_send(                       \
-			coro, c, ERR_NEEDS_ARG, LEN_NEEDS_ARG); \
-		free(buf);                                  \
-		continue;                                   \
+#define needsArg(msg)                       \
+	if(arg == NULL) {                       \
+		zeolite_channel_send(coro, c,       \
+			(unsigned char*) ERR_NEEDS_ARG, \
+			LEN_NEEDS_ARG                   \
+		);                                  \
+		free(buf);                          \
+		continue;                           \
 	}
-
-typedef struct {
-	krk_coro_t* coro;
-	krk_eventloop_t* loop;
-	zeolite_channel* c;
-} data;
-
-typedef void  (*send_f)(data* d, char* song);
-typedef char* (*recv_f)(data* d);
-
-void printList();
-void addToPlaylist(char* song);
-void queue(data* d, void* pos, send_f);
-void clear(void** pos);
-void next(data* d, void** pos, send_f);
-void complete(char* song);
-void getTable(data* d, char* table, send_f);
-void mergeChanges(data* d, recv_f);
-void onDisconnect(void** pos);
 
 zeolite_error trustAll(zeolite_sign_pk pk) {
 	char* b64 = zeolite_enc_b64(pk, sizeof(zeolite_sign_pk));
@@ -43,27 +26,131 @@ zeolite_error trustAll(zeolite_sign_pk pk) {
 	return SUCCESS;
 }
 
-void send(data* d, char* song) {
-	zeolite_channel_send(d->coro, d->c, song, strlen(song));
+typedef void (*send_f)(krk_coro_t* coro, zeolite_channel* c, char* msg);
+void send(krk_coro_t* coro, zeolite_channel* c, char* msg) {
+	zeolite_channel_send(coro, c, (unsigned char*) msg, strlen(msg));
 }
 
-char* recv(data* d) {
+char* recv(krk_coro_t* coro, zeolite_channel* c) {
 	char*    buf = NULL;
 	uint32_t len = 0;
-	zeolite_channel_recv(d->coro, d->c, (unsigned char**) &buf, &len);
+	zeolite_channel_recv(coro, c, (unsigned char**) &buf, &len);
 	return buf;
 }
 
+typedef struct song {
+	char* name;
+	int   refcount;
+	struct song* next;
+} song;
+
+static struct {
+	song* head;
+	song* tail;
+} globalList = {0};
+
+void printList() {
+	puts("Playlist:");
+	for(song* s = globalList.head; s != NULL; s = s->next) {
+		printf("	%s (%d)\n", s->name, s->refcount);
+	}
+}
+
+void sendSong(krk_coro_t* coro, zeolite_channel* c, song* s) {
+	char* buf = malloc(strlen(s->name) + 16);
+	sprintf(buf, "%s (%d)\n", s->name, s->refcount);
+	send(coro, c, buf);
+	free(buf);
+}
+
+void addToPlaylist(char* name) {
+	song* new = malloc(sizeof(song));
+	new->name = name;
+	new->refcount = 0;
+
+	if(globalList.tail == NULL) {
+		globalList.head = new;
+		globalList.tail = new;
+	} else {
+		globalList.tail->next = new;
+		globalList.tail = new;
+	}
+}
+
+void delFirst() {
+	if(globalList.head == NULL) {
+		err(1, "delFirst called on empty list!");
+	} else {
+		song* ptr = globalList.head;
+		globalList.head = ptr->next;
+		free(ptr);
+	}
+}
+
+void addFromGenerator() {
+	addToPlaylist("generated");
+}
+
+void finish(song* pos) {
+	// Leave current song, possible deleting it if we were the last referent and
+	// and the song is the last element in the playlist
+	// we have an old element, decrease its refcount
+
+	pos->refcount--;
+	if(pos->refcount <= 0 && pos == globalList.head) {
+		delFirst();
+	}
+}
+
+void advance(song** pos) {
+	// Advances `position` to the next song in the playlist.
+	// If there is no current song selected, go to the start of the playlist.
+	// If `generateNewSong` is false, no new song will be generated
+	// when `position` is at the end of the playlist.
+
+	if(*pos == NULL) {
+		*pos = globalList.head;
+		if(*pos == NULL) {
+			addFromGenerator();
+			*pos = globalList.head;
+		}
+	} else {
+		finish(*pos);
+
+		if((*pos)->next == NULL) {
+			addFromGenerator();
+		}
+
+		*pos = (*pos)->next;
+	}
+
+	(*pos)->refcount++;
+}
+
+void clear(song** pos) {
+	if(*pos == NULL) {
+		advance(pos);
+	}
+
+	while((*pos)->next != NULL) {
+		advance(pos);
+	}
+}
+
+void complete(char* song);
+
+// void getTable(data* d, char* table, send_f);
+// void mergeChanges(data* d, recv_f);
+// void onDisconnect(song** pos);
+
 int handler(
 	krk_coro_t* coro, krk_eventloop_t* loop,
-	zeolite_channel* c, void** pos
+	zeolite_channel* c, song** pos
 ) {
-	data me = {coro, loop, c};
-
 	for(;;) {
-		unsigned char* buf = NULL;
-		uint32_t       len = 0;
-		zeolite_error e = zeolite_channel_recv(coro, c, &buf, &len);
+		char*    buf = NULL;
+		uint32_t len = 0;
+		zeolite_error e = zeolite_channel_recv(coro, c, (unsigned char**) &buf, &len);
 		if(e != SUCCESS) {
 			warnx("Could not receive: %s", zeolite_error_str(e));
 			krk_coro_error(coro);
@@ -80,31 +167,34 @@ int handler(
 			needsArg();
 			addToPlaylist(arg);
 		} else if(strcmp(buf, "queue") == 0) {
-			queue(&me, *pos, send);
+			for(song* s = *pos ? *pos : globalList.head; s != NULL; s = s->next) {
+				sendSong(coro, c, s);
+			}
+			send(coro, c, "\n");
 		} else if(strcmp(buf, "clear") == 0) {
 			clear(pos);
 		} else if(strcmp(buf, "next") == 0) {
-			next(&me, &pos, send);
+			advance(pos);
+			sendSong(coro, c, *pos);
 		} else if(strcmp(buf, "complete") == 0) {
 			needsArg();
 			complete(arg);
 		} else if(strcmp(buf, "getTable") == 0) {
 			needsArg();
-			getTable(&me, arg, send);
+			// getTable(&me, arg, send);
 		} else if(strcmp(buf, "mergeChanges") == 0) {
-			mergeChanges(&me, recv);
+			// mergeChanges(&me, recv);
 		} else {
 			const char* err = "Error: unknown command\n";
-			zeolite_channel_send(coro, c, err, strlen(err));
+			zeolite_channel_send(coro, c, (unsigned char*) err, strlen(err));
 		}
-
 	}
 
 	krk_coro_finish(coro, NULL);
 }
 
 int outer(krk_coro_t* coro, krk_eventloop_t* loop, zeolite_channel* c) {
-	void* pos = NULL;
+	song* pos = NULL;
 	krk_coro_t inner = {0};
 	krk_coro_mk(&inner, handler, 3, loop, c, &pos);
 	for(;;) {
@@ -116,11 +206,13 @@ int outer(krk_coro_t* coro, krk_eventloop_t* loop, zeolite_channel* c) {
 				krk_coro_yield(coro, NULL);
 				break;
 			case ERRORED:
-				onDisconnect(&pos);
+				// onDisconnect(&pos);
 				printList();
 				krk_coro_free(&inner);
 				krk_coro_error(coro);
 				break;
+			default:
+				err(1, "Impossible state");
 		}
 	}
 }
